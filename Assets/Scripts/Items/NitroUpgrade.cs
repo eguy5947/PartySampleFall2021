3ddf9ff5df8d075daeab674e4b08d49635427364
Untouchez <65210494+Untouchez@users.noreply.github.com>
2021-11-28 21:23:29 -0800

    Shop system (Corrina & Ryan)
    -added item class
    -added inventory to use items

---
+++ Assets/Scripts/Items/NitroUpgrade.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NitroUpgrade : Item
+{
+    public BoostManager bm;
+    public float maxBoostUpgrade;
+    public override void OnActivate()
+    {
+        bm = FindObjectOfType<BoostManager>();
+        Debug.Log("Nitro Upgrade");
+        bm.maxBoostAmount += maxBoostUpgrade;
+    }
+}
---
+++ Assets/Scripts/Items/Potion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : Item
+{
+    //Reference to which Does not Exist
+    HealthScript health;
+    public int amount;
+    public override void OnActivate()
+    {
+        health = GetComponent<HealthScript>();
+        health.hp += amount;
+        Debug.Log("Restore Health");
+    }
+}
